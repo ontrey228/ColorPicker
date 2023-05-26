9a88c584facec74dc94016a0827db588db63f50d
ontrey228 <107646495+ontrey228@users.noreply.github.com>
2023-05-26 16:33:34 +0300

    t.me/ontrey228_github
    t.me/ontrey228_github

---
+++ ColorPicker.h
@@ -0,0 +1,80 @@
+//
+//  ColoPicker.h//  ColoPicker
+//
+//  Created by ONTREY228 on 25/05/23.
+//  Copyright © 2023 ONTREY228. All rights reserved.
+//
+
+
+@import UIKit;
+
+@interface ColorPicker : UIView
+
+/* Alpha picker
+ *
+ * Boolean value indicating whether to add an RGB picker.
+ * (Default: NO)
+ */
+
+@property (nonatomic) BOOL rgbPicker;
+
+/* Alpha picker
+ *
+ * Boolean value indicating whether to add an HSB picker.
+ * (Default: NO)
+ */
+
+@property (nonatomic) BOOL hsbPicker;
+
+/* Alpha picker
+ *
+ * Boolean value indicating whether to add an alpha picker. Only used with rgbPicker or hsbPicker.
+ * (Default: NO)
+ */
+
+@property (nonatomic) BOOL alphaPicker;
+
+
+/** Get selected color
+ *
+ * Get selected color from pickers.
+ */
+
+- (UIColor *)getSelectedColor;
+
+/** Set selected color
+ *
+ * @param color UIColor set your own color.
+ */
+
+- (void)setSelectedColor:(UIColor *)color;
+
+/** Color was update
+ *
+ * Called when the color changes.
+ */
+
+- (void)colorWasUpdate:(void (^)(void))a;
+
+/** Get selected string color
+ *
+ * Get selected color in color picker converted to string.
+ */
+
+- (NSString *)getSelectedStringColor;
+
+/** NSString from UIColor
+ *
+ * @param color UIColor convert color to string.
+ */
+
++ (NSString *)NSStringFromUIColor:(UIColor *)color;
+
+/** UIColor from NSString
+ *
+ * @param color UIColor convert string to color.
+ */
+
++ (UIColor *)UIColorFromNSString:(NSString *)string;
+
+@end
---
+++ README.md
@@ -0,0 +1,39 @@
+# iOS Color picker
+
+<b>Preview: </b>
+<img src="https://ezgif.com/save/ezgif-4-3a11be3cdc.gif">
+
+### Using the menu:
+
+<b> Setup: </b>
+```
+ColorPicker *picker = [[ColorPicker alloc]initWithFrame:CGRectMake(0, 0, 300, 220)];
+picker.center = window.center;
+picker.backgroundColor = [UIColor colorWithRed:0.09f green:0.1f blue:0.11f alpha:0.9f];
+picker.rgbPicker = true; //Display three buttons to select red, green and blue colors.
+picker.hsbPicker = true; //Display three buttons to select hue, saturation and brightness.
+picker.alphaPicker = true; //Display a button to select the transparency of a color. Only works with rgbPicker or hsbPicker
+[picker setSelectedColor:[UIColor colorWithRed:0.12f green:0.34f blue:0.56f alpha:0.78f]]; //set selected colors
+[window addSubview:picker];
+```
+
+<b> Appply: </b>
+```
+UIView *view = ...
+UILabel *label = ...
+
+[picker colorWasUpdate: ^(void) {
+    view.backgroundColor = [picker getSelectedColor];
+	label.textColor = [picker getSelectedColor];
+}];
+```
+
+<b> Addinhg to the project: </b>
+```
+$(TWEAK_NAME)_LDFLAGS += ColorPicker.a
+```
+
+### Credits:
+
+* [ontrey228 tg](https://t.me/leon_tap)
+* [ontrey228 ds](https://discord.gg/leontap)
